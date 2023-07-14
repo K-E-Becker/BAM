7ff23b476abce3df74311cd898546f5297e66a83
K-E_Becker <krstn.becker@gmail.com>
2023-07-13 22:35:35 -0400

    styling

--- index.js
+++ index.js
@@ -12,32 +12,22 @@ let result = 0;
 
 const handleSwitch1 = () => {
   switch1 = !switch1;
-  updateSwitchStatus("switch1Status", switch1);
 };
 
 const handleSwitch2 = () => {
   switch2 = !switch2;
-  updateSwitchStatus("switch2Status", switch2);
 };
 
 const handleSwitch3 = () => {
   switch3 = !switch3;
-  updateSwitchStatus("switch3Status", switch3);
 };
 
 const handleSwitch4 = () => {
   switch4 = !switch4;
-  updateSwitchStatus("switch4Status", switch4);
 };
 
 const handleSwitch5 = () => {
   switch5 = !switch5;
-  updateSwitchStatus("switch5Status", switch5);
-};
-
-//updates the status of each switch based on its value
-const updateSwitchStatus = (statusId, switchValue) => {
-  document.querySelector(`#${statusId}`).textContent = switchValue ? "1" : "0";
 };
 
 const handleAddition = () => {
@@ -68,13 +58,6 @@ document.querySelector("#switch3").addEventListener("change", handleSwitch3);
 document.querySelector("#switch4").addEventListener("change", handleSwitch4);
 document.querySelector("#switch5").addEventListener("change", handleSwitch5);
 document.querySelector("#addButton").addEventListener("click", handleAddition);
-
-// Set initial switch status
-updateSwitchStatus("switch1Status", switch1);
-updateSwitchStatus("switch2Status", switch2);
-updateSwitchStatus("switch3Status", switch3);
-updateSwitchStatus("switch4Status", switch4);
-updateSwitchStatus("switch5Status", switch5);
 
 function updateDom() {
   document.querySelector("#result").textContent = result;
