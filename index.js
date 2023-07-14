6f879d5afffa76218be442fc5a1a6766ac341f66
K-E_Becker <krstn.becker@gmail.com>
2023-07-14 19:23:16 -0400

    add binary and add decimal

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ let switch2 = false;
 let switch3 = false;
 let switch4 = false;
 let switch5 = false;
+let switch6 = false;
+let switch7 = false;
+let switch8 = false;
+let switch9 = false;
+let switch10 = false;
 let carry = false;
 let result = 0;
 
@@ -30,26 +35,75 @@ const handleSwitch5 = () => {
   switch5 = !switch5;
 };
 
-const handleAddition = () => {
-  // add switch values
-  let sum =
-    (switch1 ? 1 : 0) +
-    (switch2 ? 1 : 0) +
-    (switch3 ? 1 : 0) +
-    (switch4 ? 1 : 0) +
-    (switch5 ? 1 : 0);
+const handleSwitch6 = () => {
+  switch6 = !switch6;
+};
 
-  // is there a carry
-  let newCarry = sum > 1;
+const handleSwitch7 = () => {
+  switch7 = !switch7;
+};
 
-  // decides the new Sum and keeping it in binary
-  let newSum = sum % 2;
+const handleSwitch8 = () => {
+  switch8 = !switch8;
+};
 
-  carry = newCarry;
-  result = newSum;
+const handleSwitch9 = () => {
+  switch9 = !switch9;
+};
 
-  // this fixed a performance issue
-  setTimeout(updateDom, 0);
+const handleSwitch10 = () => {
+  switch10 = !switch10;
+};
+
+const getSwitchValue1 = () => {
+  let output = "";
+
+  if (switch1) output += "1";
+  else output += "0";
+
+  if (switch2) output += "1";
+  else output += "0";
+
+  if (switch3) output += "1";
+  else output += "0";
+
+  if (switch4) output += "1";
+  else output += "0";
+
+  if (switch5) output += "1";
+  else output += "0";
+
+  return output;
+};
+
+const getSwitchValue2 = () => {
+  let output = "";
+
+  if (switch6) output += "1";
+  else output += "0";
+
+  if (switch7) output += "1";
+  else output += "0";
+
+  if (switch8) output += "1";
+  else output += "0";
+
+  if (switch9) output += "1";
+  else output += "0";
+
+  if (switch10) output += "1";
+  else output += "0";
+
+  return output;
+};
+
+const addBinaryNumbers = (switchValue1, switchValue2) => {
+  const decimal1 = parseInt(switchValue1, 2);
+  const decimal2 = parseInt(switchValue2, 2);
+  const decimalSum = decimal1 + decimal2;
+  const binarySum = decimalSum.toString(2);
+
+  return binarySum;
 };
 
 document.querySelector("#switch1").addEventListener("change", handleSwitch1);
@@ -57,8 +111,40 @@ document.querySelector("#switch2").addEventListener("change", handleSwitch2);
 document.querySelector("#switch3").addEventListener("change", handleSwitch3);
 document.querySelector("#switch4").addEventListener("change", handleSwitch4);
 document.querySelector("#switch5").addEventListener("change", handleSwitch5);
-document.querySelector("#addButton").addEventListener("click", handleAddition);
+document.querySelector("#switch6").addEventListener("change", handleSwitch6);
+document.querySelector("#switch7").addEventListener("change", handleSwitch7);
+document.querySelector("#switch8").addEventListener("change", handleSwitch8);
+document.querySelector("#switch9").addEventListener("change", handleSwitch9);
+document.querySelector("#switch10").addEventListener("change", handleSwitch10);
+document.querySelector("#addButton").addEventListener("click", () => {
+  const switchValue1 = getSwitchValue1();
+  const switchValue2 = getSwitchValue2();
+  const totalBinary = addBinaryNumbers(switchValue1, switchValue2);
+  const decimalNumber1 = parseInt(switchValue1, 2);
+  const decimalNumber2 = parseInt(switchValue2, 2);
+  const totalDecimal = parseInt(totalBinary, 2);
+  updateDom(
+    switchValue1,
+    switchValue2,
+    totalBinary,
+    decimalNumber1,
+    decimalNumber2,
+    totalDecimal
+  );
+});
 
-function updateDom() {
-  document.querySelector("#result").textContent = result;
+function updateDom(
+  switchValue1,
+  switchValue2,
+  totalBinary,
+  decimalNumber1,
+  decimalNumber2,
+  totalDecimal
+) {
+  document.querySelector("#resultRow1").textContent = switchValue1;
+  document.querySelector("#resultRow2").textContent = switchValue2;
+  document.querySelector("#totalBinary").textContent = totalBinary;
+  document.querySelector("#decimalResult1").textContent = decimalNumber1;
+  document.querySelector("#decimalResult2").textContent = decimalNumber2;
+  document.querySelector("#totalDecimal").textContent = totalDecimal;
 }
