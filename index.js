1299e08e619f30dfa0b683d4da4dd8565627d253
K-E_Becker <krstn.becker@gmail.com>
2023-07-13 21:49:02 -0400

    setting initial status

--- index.js
+++ index.js
@@ -9,46 +9,56 @@ let carry = false;
 let result = 0;
 
 //handle the switch between on and off for switch 1
+
 const handleSwitch1 = () => {
   switch1 = !switch1;
+  updateSwitchStatus("switch1Status", switch1);
 };
-//handle the switch between on and off for switch 2
+
 const handleSwitch2 = () => {
   switch2 = !switch2;
+  updateSwitchStatus("switch2Status", switch2);
 };
-//handle the switch between on and off for switch 3
+
 const handleSwitch3 = () => {
   switch3 = !switch3;
+  updateSwitchStatus("switch3Status", switch3);
 };
-//handle the switch between on and off for switch 4
+
 const handleSwitch4 = () => {
   switch4 = !switch4;
+  updateSwitchStatus("switch4Status", switch4);
 };
-//handle the switch between on and off for switch 5
+
 const handleSwitch5 = () => {
   switch5 = !switch5;
+  updateSwitchStatus("switch5Status", switch5);
+};
+
+//updates the status of each switch based on its value
+const updateSwitchStatus = (statusId, switchValue) => {
+  document.querySelector(`#${statusId}`).textContent = switchValue ? "1" : "0";
 };
 
 const handleAddition = () => {
-  //add switch1, switch2, and if those are over 1...then a carry
+  // add switch values
   let sum =
     (switch1 ? 1 : 0) +
     (switch2 ? 1 : 0) +
     (switch3 ? 1 : 0) +
     (switch4 ? 1 : 0) +
-    (switch5 ? 1 : 0) +
-    (carry ? 1 : 0);
+    (switch5 ? 1 : 0);
 
-  //is there a carry
+  // is there a carry
   let newCarry = sum > 1;
 
-  //decides the new Sum and keeping it in binary
+  // decides the new Sum and keeping it in binary
   let newSum = sum % 2;
 
   carry = newCarry;
   result = newSum;
 
-  //this fixed a performance issue
+  // this fixed a performance issue
   setTimeout(updateDom, 0);
 };
 
@@ -58,6 +68,13 @@ document.querySelector("#switch3").addEventListener("change", handleSwitch3);
 document.querySelector("#switch4").addEventListener("change", handleSwitch4);
 document.querySelector("#switch5").addEventListener("change", handleSwitch5);
 document.querySelector("#addButton").addEventListener("click", handleAddition);
+
+// Set initial switch status
+updateSwitchStatus("switch1Status", switch1);
+updateSwitchStatus("switch2Status", switch2);
+updateSwitchStatus("switch3Status", switch3);
+updateSwitchStatus("switch4Status", switch4);
+updateSwitchStatus("switch5Status", switch5);
 
 function updateDom() {
   document.querySelector("#result").textContent = result;
