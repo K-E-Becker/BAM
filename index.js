0c8cb5d45e21a6384d3babadf72579071b10ceef
K-E_Becker <krstn.becker@gmail.com>
2023-07-15 14:40:22 -0400

    multiplication

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ const getSwitchValue2 = () => {
 };
 
 const addBinaryNumbers = (switchValue1, switchValue2) => {
-  const decimal1 = parseInt(switchValue1, 2);
-  const decimal2 = parseInt(switchValue2, 2);
+  const decimal1 = BigInt(parseInt(switchValue1, 2));
+  const decimal2 = BigInt(parseInt(switchValue2, 2));
   const decimalSum = decimal1 + decimal2;
   const binarySum = decimalSum.toString(2);
 
@@ -42,6 +42,15 @@ const subtractBinaryNumbers = (switchValue1, switchValue2) => {
   const binarySum = decimalSum.toString(2);
 
   return binarySum;
+};
+
+const multiplyBinaryNumbers = (switchValue1, switchValue2) => {
+  const decimal1 = BigInt(parseInt(switchValue1, 2));
+  const decimal2 = BigInt(parseInt(switchValue2, 2));
+  const decimalProduct = decimal1 * decimal2;
+  const binaryProduct = decimalProduct.toString(2);
+
+  return binaryProduct;
 };
 
 const updateLightBulbs = (totalBinary) => {
@@ -106,6 +115,8 @@ document.querySelector("#addButton").addEventListener("click", () => {
     totalBinary = addBinaryNumbers(switchValue1, switchValue2);
   } else if (operation === "-") {
     totalBinary = subtractBinaryNumbers(switchValue1, switchValue2);
+  } else if (operation === "*") {
+    totalBinary = multiplyBinaryNumbers(switchValue1, switchValue2);
   } else {
     // Handle invalid operation
     return;
